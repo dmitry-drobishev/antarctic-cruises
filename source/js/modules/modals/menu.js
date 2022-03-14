3308d43f3bc9cab2bff4d9f5201bd021a31e6b58
stooped-dog <mitya.drobishev.77rus@yandex.ru>
2022-03-14 18:26:25 +0300

    доделал header, блок about, поправил js

--- source/js/modules/modals/menu.js
+++ source/js/modules/modals/menu.js
@@ -1,19 +1,23 @@
 const openMenu = () => {
-  const navMain = document.querySelector('.header-nav');
+  const navMain = document.querySelector('.header__nav');
   const navToggle = document.querySelector('.header__toggle');
-  navMain.classList.remove('header-nav--nojs');
+  const headerLogo = document.querySelector('.header__logo');
+
+  navMain.classList.remove('header__nav--nojs');
   navToggle.classList.remove('header__toggle--nojs');
   navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('header-nav--close')) {
-      navMain.classList.remove('header-nav--close');
-      navMain.classList.add('header-nav--open');
+    if (navMain.classList.contains('header__nav--close')) {
+      navMain.classList.remove('header__nav--close');
+      navMain.classList.add('header__nav--open');
       navToggle.classList.remove('header__toggle--close');
       navToggle.classList.add('header__toggle--open');
+      headerLogo.classList.add('header__logo--hidden');
     } else {
-      navMain.classList.add('header-nav--close');
-      navMain.classList.remove('header-nav--open');
+      navMain.classList.add('header__nav--close');
+      navMain.classList.remove('header__nav--open');
       navToggle.classList.add('header__toggle--close');
       navToggle.classList.remove('header__toggle--open');
+      headerLogo.classList.remove('header__logo--hidden');
     }
   });
 };
